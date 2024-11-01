b4e1b00c2021b2987af6e3fbb652adbdf6f6b2d1
JW HWANG <kgwjddn544@gmail.com>
2024-11-01 22:21:44 +0900

    Title: 최소공배수 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241101/최소공배수 구하기/find-the-least-common-multiple.cpp
+++ 241101/최소공배수 구하기/find-the-least-common-multiple.cpp
@@ -1,29 +1,27 @@
 #include <iostream>
-using namespace std;
+#include <cstdlib> // abs 함수를 사용하기 위한 헤더
 
-void max(int n, int m) {
-    int answer;
-    int small;
-    for (int i = 2; i < min(n, m); i++) {
-        if (n % i == 0 && m % i == 0) {
-            small = i;
-            break;
-        }
+// 최소 공배수를 계산하고 출력하는 함수
+void calculateAndPrintLcm(int n, int m) {
+    // 최대 공약수(GCD) 계산
+    int a = n, b = m;
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
     }
-    for (int i = 1; i <100000; i++) {
-        int before = small * i;
-        if (before % n == 0 && before % m == 0) {
-            answer = before;
-            break;
-        }
-    }
-    cout << answer;
+    int gcd = a;
+
+    // 최소 공배수(LCM) 계산
+    int lcm = (n == 0 || m == 0) ? 0 : abs(n * m) / gcd;
+
+    // 결과 출력
+    std::cout << lcm << std::endl;
 }
 
 int main() {
-    // 여기에 코드를 작성해주세요.
     int n, m;
-    cin >> n >> m;
-    max(n, m);
+    std::cin >> n >> m;
+    calculateAndPrintLcm(n, m); // LCM 계산 및 출력
     return 0;
 }
