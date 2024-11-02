9de4d7b928725e398e4b49a0f82cffe1557a8081
JW HWANG <kgwjddn544@gmail.com>
2024-11-02 17:07:19 +0900

    Title: 두 정수에 대한 연산값, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241102/두 정수에 대한 연산값/operational-values-for-two-integers.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+void Program(int &a, int &b){
+    if(a>b){
+        a += 25;
+        b *= 2;
+    }
+    else{
+        b +=25;
+        a *= 2;
+    }
+}
+
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a,b;
+    cin>>a>>b;
+    Program(a,b);
+    cout<<a<<" "<<b;
+    return 0;
+}
