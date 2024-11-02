53c28c793a99f222269f03a910c135743e8f7420
JW HWANG <kgwjddn544@gmail.com>
2024-11-02 14:48:59 +0900

    Title: 함수를 이용한 소수 판별, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241102/함수를 이용한 소수 판별/decimal-decisions-using-functions.cpp
+++ 241102/함수를 이용한 소수 판별/decimal-decisions-using-functions.cpp
@@ -1,33 +1,28 @@
 #include <iostream>
+
 using namespace std;
 
-bool Find_(int n){
-    for(int i = 2; i<n; i++){
-        if(n % i == 0){
+bool IsPrime(int n) {
+    if(n == 1)
+        return false;
+
+    for(int i = 2; i < n; i++)
+        if(n % i == 0)
             return false;
-        }
-    }
+
     return true;
 }
 
-int Answer_Sum(int n, int m){
-    int sum = 0;
-    if(n == 0) sum = -1;
-    for(int i = n; i<=m; i++){
-        if(Find_(i) == true){
-            sum += i;
-        }
-    }
-    return sum;
-}
-
-
 int main() {
-    // 여기에 코드를 작성해주세요.
-    int n,m, answer;
-    cin>> n>>m;
-    answer = Answer_Sum(n,m);
-    cout<<answer;
+    // 변수 선언 및 입력:
+    int a, b;
+    cin >> a >> b;
 
+    int sum = 0;
+    for(int i = a; i <= b; i++)
+        if(IsPrime(i))
+            sum += i;
+    
+    cout << sum;
     return 0;
 }
