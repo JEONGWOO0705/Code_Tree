b0a03ff261a2a35221446f12de4273bca961d8a5
JW HWANG <kgwjddn544@gmail.com>
2024-11-03 17:57:09 +0900

    Title: 재귀함수를 이용한 별 출력 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241103/재귀함수를 이용한 별 출력 2/star-output-with-recursive-function-2.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+
+// 재귀함수를 이용해 숫자를 n부터 1까지 출력하고, 다시 1부터 n까지 출력합니다.
+void PrintStar(int n) {
+    if(n == 0)
+        return;
+    for(int i = 0; i<n; i++){
+        cout << "*" << " ";
+    }
+    cout<<endl;
+    PrintStar(n - 1);
+    for(int i = 0; i<n; i++){
+        cout << "*" << " ";
+    }
+    cout<<endl;
+}
+
+
+int main() {
+    // 변수 선언 및 입력:
+    int n;
+    cin >> n;
+
+    PrintStar(n);
+
+    return 0;
+}
