77dbbf4f0f2e530b49e25eecacb791ab7ad8594b
JW HWANG <kgwjddn544@gmail.com>
2024-11-19 21:25:29 +0900

    Title: 연속되는 수 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241119/연속되는 수 2/continuous-number2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+#define MAX 50
+int _array[MAX];
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+    int ab;
+    int cnt = 1;
+    for (int i = 0; i < n; i++) {
+        cin >> ab;
+        _array[i] = ab;
+    }
+    for (int j = 0; j <= n - 1; j++) {
+        if (_array[j] == _array[j + 1]) {
+            cnt++;
+        }
+    }
+    if (cnt == 1) {
+        cnt = 0;
+    }
+    cout << cnt;
+    return 0;
+}
